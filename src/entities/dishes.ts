2e169023d45b731d9873e7d02e0534300ce0cbd4
asem210 <gustavoadrianpazos@gmail.com>
2022-08-14 16:59:11 -0500

    api dishes connect

---
+++ src/entities/dishes.ts
@@ -0,0 +1,9 @@
+export interface DishesDefault {
+  id: number;
+  nombre: string;
+  descripcion: string;
+  imagen: string;
+  precio: number;
+  id_categoria: number;
+  id_oferta: number;
+}
---
+++ src/services/dishes.ts
@@ -0,0 +1,49 @@
+import axios from "axios";
+import { DishesDefault } from "../entities/dishes";
+
+const BASE_URL = "http://localhost:5000";
+
+const dishesService = {
+  list: async (): Promise<DishesDefault[] | null> => {
+    try {
+      const { data } = await axios.get(`${BASE_URL}/dishes/all`);
+      return data.data;
+    } catch (error) {
+      return null;
+    }
+  },
+
+  count: async () => {
+    try {
+      const { data } = await axios.get(`${BASE_URL}/dishes/count`);
+      return data.data;
+    } catch (error) {
+      return null;
+    }
+  },
+
+  create: async (
+    name: string,
+    description: string,
+    image: string,
+    price: number,
+    idcategory: number
+  ) => {
+    try {
+      const { data } = await axios({
+        url: `${BASE_URL}/category/create`,
+        method: "post",
+        data: {
+          name: name,
+          description: description,
+          image: image,
+          price: price,
+          idcategory: idcategory,
+        },
+      });
+      return data;
+    } catch (error) {
+      return null;
+    }
+  },
+};
